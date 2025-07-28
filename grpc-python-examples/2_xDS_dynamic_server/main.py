aceb6ea67d726945cb2670110593eec5716b643e
eunsang <thecynicdog0328@gmail.com>
2025-07-28 22:56:00 +0900

    Update main.py

--- grpc-python-examples/2_xDS_dynamic_server/main.py
+++ grpc-python-examples/2_xDS_dynamic_server/main.py
@@ -1,6 +1,94 @@
-def main():
-    print("Hello from 2-xds-dynamic-server!")
+import grpc
+from concurrent import futures
+import time
 
+# Import generated protobuf classes here
+from envoy.service.discovery.v3 import ads_pb2_grpc, ads_pb2
+from envoy.config.listener.v3 import listener_pb2
+from envoy.config.route.v3 import route_pb2
+from envoy.config.cluster.v3 import cluster_pb2
+from envoy.config.endpoint.v3 import endpoint_pb2
+from google.protobuf import any_pb2
 
-if __name__ == "__main__":
-    main()
+class SimpleADS(ads_pb2_grpc.AggregatedDiscoveryServiceServicer):
+    def StreamAggregatedResources(self, request_iterator, context):
+        # This method is bi-directional stream
+        for req in request_iterator:
+            # Check req.type_url and respond accordingly
+            if req.type_url == "type.googleapis.com/envoy.config.listener.v3.Listener":
+                # Build Listener proto
+                listener = build_listener()
+                resp = ads_pb2.DiscoveryResponse(
+                    version_info="1",
+                    resources=[any_pb2.Any().Pack(listener)],
+                    type_url=req.type_url,
+                    nonce="1"
+                )
+                yield resp
+
+            elif req.type_url == "type.googleapis.com/envoy.config.route.v3.RouteConfiguration":
+                route_config = build_route()
+                resp = ads_pb2.DiscoveryResponse(
+                    version_info="1",
+                    resources=[any_pb2.Any().Pack(route_config)],
+                    type_url=req.type_url,
+                    nonce="1"
+                )
+                yield resp
+
+            elif req.type_url == "type.googleapis.com/envoy.config.cluster.v3.Cluster":
+                cluster = build_cluster()
+                resp = ads_pb2.DiscoveryResponse(
+                    version_info="1",
+                    resources=[any_pb2.Any().Pack(cluster)],
+                    type_url=req.type_url,
+                    nonce="1"
+                )
+                yield resp
+
+            elif req.type_url == "type.googleapis.com/envoy.config.endpoint.v3.ClusterLoadAssignment":
+                endpoints = build_endpoints()
+                resp = ads_pb2.DiscoveryResponse(
+                    version_info="1",
+                    resources=[any_pb2.Any().Pack(endpoints)],
+                    type_url=req.type_url,
+                    nonce="1"
+                )
+                yield resp
+
+def build_listener():
+    # Construct Listener proto matching your simple.yaml listener on 0.0.0.0:15001
+    # with HTTP connection manager and route to 'httpbin_local_route'
+    # ...
+    pass
+
+def build_route():
+    # Construct RouteConfiguration with a virtual host matching "/" prefix
+    # and routes to cluster "httpbin_service"
+    # ...
+    pass
+
+def build_cluster():
+    # Construct Cluster named "httpbin_service", logical DNS type pointing to "httpbin:8000"
+    # ...
+    pass
+
+def build_endpoints():
+    # Construct ClusterLoadAssignment resolving "httpbin" (can be static IP or DNS)
+    # ...
+    pass
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    ads_pb2_grpc.add_AggregatedDiscoveryServiceServicer_to_server(SimpleADS(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    print("xDS server started on 50051")
+    try:
+        while True:
+            time.sleep(86400)
+    except KeyboardInterrupt:
+        server.stop(0)
+
+if __name__ == '__main__':
+    serve()
